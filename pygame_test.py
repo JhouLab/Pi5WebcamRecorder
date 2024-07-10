9860c656f9cb8d5eac4a1370e895d8a148049f2b
Thomas Jhou <tomjhou@gmail.com>
2024-07-10 00:47:12 -0400

    Added pygame test. This is a (likely to be abandoned) attempt to speed up video rendering in tkinter window.

---
+++ pygame_test.py
@@ -0,0 +1,37 @@
+import os
+import pygame
+import tkinter as tk
+from tkinter import *
+import time
+
+root = tk.Tk()
+embed = tk.Frame(root, width=500, height=500) #creates embed frame for pygame window
+embed.grid(columnspan=600, rowspan=500) # Adds grid
+embed.pack(side=LEFT) #packs window to the left
+buttonwin = tk.Frame(root, width=75, height=500)
+buttonwin.pack(side=LEFT)
+
+os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
+os.environ['SDL_VIDEODRIVER'] = 'windib'
+
+# pygame.init()
+pygame.display.init()
+screen = pygame.display.set_mode((500, 500))
+screen.fill((255, 155, 55))   # pygame.Color(255, 155, 55))
+pygame.display.update()
+
+
+def draw():
+    pygame.draw.circle(screen, (50, 50, 100), (250, 250), 125)
+    pygame.display.update()
+
+
+button1 = Button(buttonwin, text='Draw',  command=draw)
+button1.pack(side=LEFT)
+root.update()
+
+while True:
+    pygame.display.update()
+    root.update()
+    time.sleep(0.001)
+
