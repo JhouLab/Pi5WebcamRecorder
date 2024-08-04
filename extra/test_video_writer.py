dd796e21fcec1d51053147eadbd69543b864b8a7
JhouLab <81326996+JhouLab@users.noreply.github.com>
2024-08-04 00:22:11 -0400

    Updated FFMPEG bug test code.

--- extra/test_video_writer.py
+++ extra/test_video_writer.py
@@ -1,3 +1,10 @@
+#
+# Test h264 codec bug
+#
+# It appears that when writing h264 video, the last frame is lost.
+# From what I can tell online, this is a bug in FFMPEG.
+#
+
 import numpy as np
 import cv2
 
@@ -10,14 +17,28 @@ IS_WINDOWS = (PLATFORM == 'windows')
 xsp=640
 ysp=480
 chan=3
+
+USE_H264 = True
+
 # Define the codec and create VideoWriter object
 if IS_WINDOWS:
+    # Windows lacks H264 codec
     fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 elif IS_LINUX:
-    fourcc = cv2.VideoWriter_fourcc(*'h264')
+    
+    if USE_H264:
+        fourcc = cv2.VideoWriter_fourcc(*'h264')
+    else:       
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 
-out = cv2.VideoWriter('output.avi', fourcc, 20.0, (xsp, ysp), True)
+out = cv2.VideoWriter('output.avi', fourcc, 10.0, (xsp, ysp), True)
 frame = np.zeros((ysp, xsp, chan), np.uint8)
-for i in range(20):   # init frames
+frame[:, :, 2] = 128  # Make red frames
+frame2 = np.zeros((ysp, xsp, chan), np.uint8)
+frame2[:, :, 1] = 128  # Make green frames
+
+for i in range(30):   # init frames
     out.write(frame)
+    
+out.write(frame2)
 out.release()
