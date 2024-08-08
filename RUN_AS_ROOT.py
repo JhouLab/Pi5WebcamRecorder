28fb0880b0d47d70e7f57a58e4d021cbc752523f
JhouLab <81326996+JhouLab@users.noreply.github.com>
2024-08-08 14:42:42 -0400

    Minor update to warning message.

--- RUN_AS_ROOT.py
+++ RUN_AS_ROOT.py
@@ -1,5 +1,5 @@
 #
-# This file runs WEBCAM_RECORD as root (superuser),
+# This file runs WEBCAM_RECORD.py as root (superuser),
 # thereby allowing it to raise its own process priority
 # for better latency performance.
 #
@@ -12,7 +12,8 @@ IS_LINUX = (PLATFORM == 'linux')
 IS_WINDOWS = (PLATFORM == 'windows')
 
 if IS_WINDOWS:
-    print("This script only works on Linux. Sorry.")
+    print("This script is only necessary on Linux. If running on Windows, please run WEBCAM_RECORD.py directly")
 elif IS_LINUX:
     wd = os.getcwd()
+    # Now run WEBCAM_RECORD.py as superuser/root. The next instruction will block until user exits the program.
     os.system("cd " + wd + "; sudo python -m WEBCAM_RECORD")
